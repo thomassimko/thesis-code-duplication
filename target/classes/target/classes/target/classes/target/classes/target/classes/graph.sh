1f3caae45e3ec4bebba4b4b164c0bc72bf5e72d4
Thomas Simko <tjsimko@gmail.com>
2019-04-02 14:45:08 -0700

    removing target

--- target/classes/target/classes/target/classes/target/classes/target/classes/graph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dot -Tpng cfg.gv -o cfg.png
-
-for filename in ./pdg/source/*.gv; do
-    name=$(basename "$filename" .gv)
-    dot -Tpng "$filename" -o ./pdg/images/"$name".png
-done
